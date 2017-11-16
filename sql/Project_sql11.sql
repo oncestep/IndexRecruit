b3c4eafe4108ed8a1d813b2da064a965694a4861
babycoder <babycoder@foxmail.com>
2017-11-16 17:21:57 +0800

    IndexRecruit Release 2.0

--- sql/Project_sql11.sql
+++ sql/Project_sql11.sql
@@ -18,6 +18,33 @@ USE `recruit`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `userid` bigint(20) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员信息';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (19980208,'17806228320','9136f8f230e20ca9afc410d14c826586','管理员','lb19961218@163.com');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `application`
 --
 
@@ -32,7 +59,7 @@ CREATE TABLE `application` (
   `positionId` int(11) NOT NULL,
   `hrId` int(11) DEFAULT NULL,
   PRIMARY KEY (`applicationId`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +68,7 @@ CREATE TABLE `application` (
 
 LOCK TABLES `application` WRITE;
 /*!40000 ALTER TABLE `application` DISABLE KEYS */;
-INSERT INTO `application` VALUES (1,1,NULL,1,1,1),(2,1,NULL,2,5,2),(3,1,NULL,2,11,2),(4,1,NULL,2,15,3),(5,1,NULL,6,11,5),(6,1,NULL,5,3,3),(7,1,NULL,6,18,1),(8,1,NULL,4,5,1),(9,1,NULL,4,13,6),(10,0,'2017-11-09 12:57:37',16,1,NULL);
+INSERT INTO `application` VALUES (1,1,NULL,1,1,1),(2,1,NULL,2,5,2),(3,1,NULL,2,11,2),(4,1,NULL,2,15,3),(5,1,NULL,6,11,5),(6,1,NULL,5,3,3),(7,1,NULL,6,18,1),(8,1,NULL,4,5,1),(9,1,NULL,4,13,6),(10,0,'2017-11-09 12:57:37',16,1,NULL),(11,0,'2017-11-16 15:10:26',16,6,NULL);
 /*!40000 ALTER TABLE `application` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +113,7 @@ CREATE TABLE `comment` (
   `userId` int(11) NOT NULL,
   `positionId` int(11) NOT NULL,
   PRIMARY KEY (`commentId`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +122,7 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES (1,3,'C++是C语言的继承，它既可以进行C语言的过程化程序设计，又可以进行以抽象数据类型为特点的基于对象的程序设计，还可以进行以继承和多态为特点的面向对象的程序设计','2017-11-14 22:47:42',1,5),(2,2,'Java是一门面向对象编程语言，不仅吸收了C++语言的各种优点，还摒弃了C++里难以理解的多继承、指针等概念','2017-11-14 22:47:42',1,17),(3,1,'Python是纯粹的自由软件， 源代码和解释器CPython遵循 GPL(GNU General Public License)协议','2017-11-14 22:47:42',2,11),(4,3,'C++不仅拥有计算机高效运行的实用性特征，同时还致力于提高大规模程序的编程质量与程序设计语言的问题描述能力','2017-11-14 22:47:42',3,18),(5,3,'这是一些关于Java工程师的评论，这个职位需要丰富的阅历的和工作经验','2017-11-14 22:47:42',5,2),(6,2,'又要写测试评论，写点啥呢？布吉岛','2017-11-14 22:47:42',6,8),(7,3,'数据挖掘一般是指从大量的数据中通过算法搜索隐藏于其中信息的过程','2017-11-14 22:47:42',6,14),(8,3,'Java具有简单性、面向对象、分布式、健壮性、安全性、平台独立与可移植性、多线程、动态性等特点','2017-11-14 22:47:42',6,17),(9,2,'Python 已经成为最受欢迎的程序设计语言之一','2017-11-14 22:47:42',1,10),(10,3,'.NET是 Microsoft XML Web services 平台','2017-11-14 22:47:42',2,9),(13,1,'评论一哈。。。PHP是最好的编程语言！','2017-11-14 22:47:42',16,7),(14,2,'Java是面向对象的编程语言，而其AOP机制面向切面编程，弥补了Java本身的缺点','2017-11-14 22:51:06',16,1),(16,3,'数据挖掘指通过挖掘算法发现隐藏在大量数据中的有价值信息点','2017-11-15 20:45:33',16,15);
+INSERT INTO `comment` VALUES (1,3,'C++是C语言的继承，它既可以进行C语言的过程化程序设计，又可以进行以抽象数据类型为特点的基于对象的程序设计，还可以进行以继承和多态为特点的面向对象的程序设计','2017-11-14 22:47:42',1,5),(2,2,'Java是一门面向对象编程语言，不仅吸收了C++语言的各种优点，还摒弃了C++里难以理解的多继承、指针等概念','2017-11-14 22:47:42',1,17),(3,1,'Python是纯粹的自由软件， 源代码和解释器CPython遵循 GPL(GNU General Public License)协议','2017-11-14 22:47:42',2,11),(4,3,'C++不仅拥有计算机高效运行的实用性特征，同时还致力于提高大规模程序的编程质量与程序设计语言的问题描述能力','2017-11-14 22:47:42',3,18),(5,3,'这是一些关于Java工程师的评论，这个职位需要丰富的阅历的和工作经验','2017-11-14 22:47:42',5,2),(6,2,'又要写测试评论，写点啥呢？布吉岛','2017-11-14 22:47:42',6,8),(7,3,'数据挖掘一般是指从大量的数据中通过算法搜索隐藏于其中信息的过程','2017-11-14 22:47:42',6,14),(8,3,'Java具有简单性、面向对象、分布式、健壮性、安全性、平台独立与可移植性、多线程、动态性等特点','2017-11-14 22:47:42',6,17),(9,2,'Python 已经成为最受欢迎的程序设计语言之一','2017-11-14 22:47:42',1,10),(10,3,'.NET是 Microsoft XML Web services 平台','2017-11-14 22:47:42',2,9),(13,1,'评论一哈。。。PHP是最好的编程语言！','2017-11-14 22:47:42',16,7),(14,2,'Java是面向对象的编程语言，而其AOP机制面向切面编程，弥补了Java本身的缺点','2017-11-14 22:51:06',16,1),(16,3,'数据挖掘指通过挖掘算法发现隐藏在大量数据中的有价值信息点','2017-11-15 20:45:33',16,15),(17,3,'评论一哈~','2017-11-16 15:10:15',16,6),(18,2,'对Python职位的一点评论。。。','2017-11-16 17:09:35',16,12);
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +192,7 @@ CREATE TABLE `favor` (
   `userId` int(11) NOT NULL,
   `positionId` int(11) NOT NULL,
   PRIMARY KEY (`favorId`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -240,7 +267,7 @@ CREATE TABLE `position` (
 
 LOCK TABLES `position` WRITE;
 /*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (1,'Java工程师','熟练使用RPC框架，具备相关的分布式开发经验',3,'北京市',12000,7000,'2017-10-27',NULL,1,136,1,1,1),(2,'Java工程师','接收应届实习生，实习期满应聘上岗接收应届实习生，实习期满应聘上岗收应届实习生，实习期满应聘上岗',5,'上海市',16000,11000,'2017-11-07',NULL,1,108,1,4,2),(3,'Java工程师','有扎实的java基础，熟悉分布式、缓存、异步消息等原理和应用',15,'天津市',13000,10000,'2017-09-30',NULL,1,3,1,7,3),(4,'Java工程师','JAVA WEB方向2年+经验',2,'南京市',16000,12000,'2017-09-25',NULL,1,7,1,10,4),(5,'C++工程师','可接收计算机相关专业应届生，表现优秀者加薪转正',30,'南京市',8000,5000,'2017-10-16',NULL,1,13,2,10,4),(6,'C++工程师','3-5年工作经验，计算机相关专业毕业',1,'上海市',16000,8000,'2017-11-03',NULL,1,0,2,4,2),(7,'PHP工程师','一年以上PHP开发经验 （项目经验丰富的，也可以升级为高级开发工程师）',10,'上海市',11000,8000,'2017-11-10',NULL,1,71,3,4,2),(8,'PHP工程师','熟悉LNMP/WNMP开发环境 , 熟练使用Yaf, Yii, ThinkPHP等一种或多种框架',5,'上海市',12000,7000,'2017-11-01',NULL,1,1,3,4,2),(9,'.NET工程师','熟悉WinForm/WPF窗体开发并有实际项目经验',2,'北京市',13000,11000,'2017-11-02',NULL,1,1,4,1,1),(10,'Python数据分析','熟练使用Linux，可以快速上手编写shell、powershell、cmd等操作系统脚本',2,'北京市',23000,18000,'2017-10-09',NULL,1,42,5,1,1),(11,'Python开发','4年以上互联网产品后台研发经验，2年以上后台构架经验',2,'北京市',22000,18000,'2017-09-23',NULL,1,0,5,1,1),(12,'Python开发','精通Python，2年或以上Python项目经验',3,'天津市',16000,14000,'2017-07-27',NULL,1,27,5,7,3),(13,'数据挖掘工程师','熟悉 Linux平台上的编程环境，精通Java开发，精通 Python/Shell等脚本语言',12,'天津市',22000,15000,'2017-11-05',NULL,1,1,6,7,3),(14,'数据挖掘工程师','熟悉Hadoop、Hive、Spark、流式计算、实时计算等大数据相关技术者优先，熟悉时序挖掘、文本挖掘、网络挖掘等优先',2,'北京市',32000,28000,'2017-11-06',NULL,1,4,6,1,1),(15,'数据挖掘工程师','精通Python，熟悉PHP/GO/Java/C++/C等语言中的一种或几种',2,'杭州市',26000,14000,'2017-11-08',NULL,1,18,6,13,5),(16,'Java工程师','熟悉Spring、Freemark、Struts、Hibernate 等开源框架',13,'杭州市',18000,15000,'2017-11-11',NULL,1,4,1,17,6),(17,'Java后端开发','熟练使用Mybatis，SpringMVC，SpringCloud等框架',5,'杭州市',21000,18000,'2017-10-23',NULL,1,3,1,13,5),(18,'C++后端开发','熟练linux系统操作，熟练gcc,gdb,vim, eclipse等开发工具',5,'北京市',12000,9000,'2017-10-28',NULL,1,0,2,1,1);
+INSERT INTO `position` VALUES (1,'Java工程师','熟练使用RPC框架，具备相关的分布式开发经验',3,'北京市',12000,7000,'2017-10-27',NULL,1,136,1,1,1),(2,'Java工程师','接收应届实习生，实习期满应聘上岗接收应届实习生，实习期满应聘上岗收应届实习生，实习期满应聘上岗',5,'上海市',16000,11000,'2017-11-07',NULL,1,108,1,4,2),(3,'Java工程师','有扎实的java基础，熟悉分布式、缓存、异步消息等原理和应用',15,'天津市',13000,10000,'2017-09-30',NULL,1,3,1,7,3),(4,'Java工程师','JAVA WEB方向2年+经验',2,'南京市',16000,12000,'2017-09-25',NULL,1,7,1,10,4),(5,'C++工程师','可接收计算机相关专业应届生，表现优秀者加薪转正',30,'南京市',8000,5000,'2017-10-16',NULL,1,13,2,10,4),(6,'C++工程师','3-5年工作经验，计算机相关专业毕业',1,'上海市',16000,8000,'2017-11-03',NULL,1,4,2,4,2),(7,'PHP工程师','一年以上PHP开发经验 （项目经验丰富的，也可以升级为高级开发工程师）',10,'上海市',11000,8000,'2017-11-10',NULL,1,71,3,4,2),(8,'PHP工程师','熟悉LNMP/WNMP开发环境 , 熟练使用Yaf, Yii, ThinkPHP等一种或多种框架',5,'上海市',12000,7000,'2017-11-01',NULL,1,1,3,4,2),(9,'.NET工程师','熟悉WinForm/WPF窗体开发并有实际项目经验',2,'北京市',13000,11000,'2017-11-02',NULL,1,1,4,1,1),(10,'Python数据分析','熟练使用Linux，可以快速上手编写shell、powershell、cmd等操作系统脚本',2,'北京市',23000,18000,'2017-10-09',NULL,1,42,5,1,1),(11,'Python开发','4年以上互联网产品后台研发经验，2年以上后台构架经验',2,'北京市',22000,18000,'2017-09-23',NULL,1,0,5,1,1),(12,'Python开发','精通Python，2年或以上Python项目经验',3,'天津市',16000,14000,'2017-07-27',NULL,1,31,5,7,3),(13,'数据挖掘工程师','熟悉 Linux平台上的编程环境，精通Java开发，精通 Python/Shell等脚本语言',12,'天津市',22000,15000,'2017-11-05',NULL,1,1,6,7,3),(14,'数据挖掘工程师','熟悉Hadoop、Hive、Spark、流式计算、实时计算等大数据相关技术者优先，熟悉时序挖掘、文本挖掘、网络挖掘等优先',2,'北京市',32000,28000,'2017-11-06',NULL,1,4,6,1,1),(15,'数据挖掘工程师','精通Python，熟悉PHP/GO/Java/C++/C等语言中的一种或几种',2,'杭州市',26000,14000,'2017-11-08',NULL,1,19,6,13,5),(16,'Java工程师','熟悉Spring、Freemark、Struts、Hibernate 等开源框架',13,'杭州市',18000,15000,'2017-11-11',NULL,1,4,1,17,6),(17,'Java后端开发','熟练使用Mybatis，SpringMVC，SpringCloud等框架',5,'杭州市',21000,18000,'2017-10-23',NULL,1,3,1,13,5),(18,'C++后端开发','熟练linux系统操作，熟练gcc,gdb,vim, eclipse等开发工具',5,'北京市',12000,9000,'2017-10-28',NULL,1,0,2,1,1);
 /*!40000 ALTER TABLE `position` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -269,7 +296,7 @@ CREATE TABLE `resume` (
 
 LOCK TABLES `resume` WRITE;
 /*!40000 ALTER TABLE `resume` DISABLE KEYS */;
-INSERT INTO `resume` VALUES (1,NULL,NULL,NULL,NULL,NULL,1),(2,NULL,NULL,NULL,NULL,NULL,2),(3,NULL,NULL,NULL,NULL,NULL,3),(4,NULL,NULL,NULL,NULL,NULL,4),(5,NULL,NULL,NULL,NULL,NULL,5),(6,NULL,NULL,NULL,NULL,NULL,6),(7,NULL,NULL,NULL,NULL,NULL,7),(8,NULL,NULL,NULL,NULL,NULL,8),(9,NULL,NULL,NULL,NULL,NULL,9),(10,NULL,NULL,NULL,NULL,NULL,10),(11,NULL,NULL,NULL,NULL,NULL,11),(12,NULL,NULL,NULL,NULL,NULL,12),(13,NULL,NULL,NULL,NULL,NULL,13),(14,NULL,NULL,NULL,NULL,NULL,14),(16,'能力很强，测试一哈','有六个月实习经历，测试一哈','无工作经历，测试一哈','有几次奖学金，测试一哈','一份稳定的工作，测试一哈',16),(17,NULL,NULL,NULL,NULL,NULL,17),(21,NULL,NULL,NULL,NULL,NULL,21);
+INSERT INTO `resume` VALUES (1,NULL,NULL,NULL,NULL,NULL,1),(2,NULL,NULL,NULL,NULL,NULL,2),(3,NULL,NULL,NULL,NULL,NULL,3),(4,NULL,NULL,NULL,NULL,NULL,4),(5,NULL,NULL,NULL,NULL,NULL,5),(6,NULL,NULL,NULL,NULL,NULL,6),(7,NULL,NULL,NULL,NULL,NULL,7),(8,NULL,NULL,NULL,NULL,NULL,8),(9,NULL,NULL,NULL,NULL,NULL,9),(10,NULL,NULL,NULL,NULL,NULL,10),(11,NULL,NULL,NULL,NULL,NULL,11),(12,NULL,NULL,NULL,NULL,NULL,12),(13,NULL,NULL,NULL,NULL,NULL,13),(14,NULL,NULL,NULL,NULL,NULL,14),(16,'能力很强，测试一哈；','有六个月实习经历，测试一哈；','无工作经历，测试一哈；','有几次的奖学金，测试一哈；','一份相对稳定的工作，测试一哈；',16),(17,NULL,NULL,NULL,NULL,NULL,17),(21,NULL,NULL,NULL,NULL,NULL,21);
 /*!40000 ALTER TABLE `resume` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -308,7 +335,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'13754258565','4QrcOUm6Wau+VuBX8g+IPg==','夏高兴',0,NULL,'云淡天高','xiagaoxin@163.com','北京市','北京市','硕士','中南财经政法大学',NULL,NULL,3),(2,'16873315255','4QrcOUm6Wau+VuBX8g+IPg==','沈茂德',0,NULL,'冷酷的云','shenmaode@163.com','北京市','北京市','本科','湖南师范大学',NULL,NULL,3),(3,'16535356412','4QrcOUm6Wau+VuBX8g+IPg==','杜文瑞',0,NULL,'我心寂寞','duwenrui@163.com','上海市','上海市','本科','东北财经大学',NULL,NULL,1),(4,'15785658371','4QrcOUm6Wau+VuBX8g+IPg==','彭友卉',0,NULL,'尘封记忆','pengyouhui@163.com','上海市','上海市','本科','西南大学',NULL,NULL,2),(5,'16735259632','4QrcOUm6Wau+VuBX8g+IPg==','崔谷槐',0,NULL,'飘雪无垠','cuiguhuai@163.com','上海市','上海市','本科','苏州大学',NULL,NULL,2),(6,'17898763255','4QrcOUm6Wau+VuBX8g+IPg==','魏茂材',0,NULL,'风过无痕','weimaocai@163.com','广东省','广州市','本科','西北大学',NULL,NULL,6),(7,'17563522636','4QrcOUm6Wau+VuBX8g+IPg==','侯成文',0,NULL,'星月相随','huochengwen','湖北省','武汉市','硕士','上海财经大学',NULL,NULL,5),(8,'18936258863','4QrcOUm6Wau+VuBX8g+IPg==','邵夜云',1,NULL,'低调沉默者','shaoyeyun@163.com','北京市','北京市','本科','江苏大学',NULL,NULL,2),(9,'13752533625','4QrcOUm6Wau+VuBX8g+IPg==','方彭湃',1,NULL,'梦醒童话','fangpengpai@163.com','天津市','天津市','硕士','西南政法大学',NULL,NULL,4),(10,'15763968252','4QrcOUm6Wau+VuBX8g+IPg==','熊新觉',1,NULL,'咖啡的味道','xiongxinjue@163.com','广东省','广州市','本科','重庆医科大学',NULL,NULL,3),(11,'13685259986','4QrcOUm6Wau+VuBX8g+IPg==','肖又香',1,NULL,'悬世尘埃','xiaoyouxiang@163.com','浙江省','杭州市','大专','福建师范大学',NULL,NULL,2),(12,'15788875236','4QrcOUm6Wau+VuBX8g+IPg==','严经纶',0,NULL,'冰封夕阳','yanjinlun@163.com','浙江省','杭州市','本科','广州中医药大学',NULL,NULL,6),(13,'18766635865','4QrcOUm6Wau+VuBX8g+IPg==','邓和豫',1,NULL,'隐水酣龙','dengheyu@163.com','浙江省','杭州市','本科','哈尔滨工程大学',NULL,NULL,1),(14,'15623527861','4QrcOUm6Wau+VuBX8g+IPg==','邓雪风',0,NULL,'一顿小皮锤','dengxuefeng@163.com','江苏省','南京市','本科','暨南大学',NULL,NULL,4),(16,'17865422925','4QrcOUm6Wau+VuBX8g+IPg==','ZLT',0,1997,'晨钟','babycoder@foxmail.com','浙江省','嘉兴市','本科','青岛大学',2019,'软件工程',5),(17,'15726928003','4QrcOUm6Wau+VuBX8g+IPg==','龟龟',0,NULL,'龟龟',NULL,NULL,NULL,NULL,NULL,NULL,NULL,0),(21,'13957336750','4QrcOUm6Wau+VuBX8g+IPg==','木木',0,NULL,'木木',NULL,NULL,NULL,NULL,NULL,NULL,NULL,0);
+INSERT INTO `user` VALUES (1,'13754258565','4QrcOUm6Wau+VuBX8g+IPg==','夏高兴',0,NULL,'云淡天高','xiagaoxin@163.com','北京市','北京市','硕士','中南财经政法大学',NULL,NULL,3),(2,'16873315255','4QrcOUm6Wau+VuBX8g+IPg==','沈茂德',0,NULL,'冷酷的云','shenmaode@163.com','北京市','北京市','本科','湖南师范大学',NULL,NULL,3),(3,'16535356412','4QrcOUm6Wau+VuBX8g+IPg==','杜文瑞',0,NULL,'我心寂寞','duwenrui@163.com','上海市','上海市','本科','东北财经大学',NULL,NULL,1),(4,'15785658371','4QrcOUm6Wau+VuBX8g+IPg==','彭友卉',0,NULL,'尘封记忆','pengyouhui@163.com','上海市','上海市','本科','西南大学',NULL,NULL,2),(5,'16735259632','4QrcOUm6Wau+VuBX8g+IPg==','崔谷槐',0,NULL,'飘雪无垠','cuiguhuai@163.com','上海市','上海市','本科','苏州大学',NULL,NULL,2),(6,'17898763255','4QrcOUm6Wau+VuBX8g+IPg==','魏茂材',0,NULL,'风过无痕','weimaocai@163.com','广东省','广州市','本科','西北大学',NULL,NULL,6),(7,'17563522636','4QrcOUm6Wau+VuBX8g+IPg==','侯成文',0,NULL,'星月相随','huochengwen','湖北省','武汉市','硕士','上海财经大学',NULL,NULL,5),(8,'18936258863','4QrcOUm6Wau+VuBX8g+IPg==','邵夜云',1,NULL,'低调沉默者','shaoyeyun@163.com','北京市','北京市','本科','江苏大学',NULL,NULL,2),(9,'13752533625','4QrcOUm6Wau+VuBX8g+IPg==','方彭湃',1,NULL,'梦醒童话','fangpengpai@163.com','天津市','天津市','硕士','西南政法大学',NULL,NULL,4),(10,'15763968252','4QrcOUm6Wau+VuBX8g+IPg==','熊新觉',1,NULL,'咖啡的味道','xiongxinjue@163.com','广东省','广州市','本科','重庆医科大学',NULL,NULL,3),(11,'13685259986','4QrcOUm6Wau+VuBX8g+IPg==','肖又香',1,NULL,'悬世尘埃','xiaoyouxiang@163.com','浙江省','杭州市','大专','福建师范大学',NULL,NULL,2),(12,'15788875236','4QrcOUm6Wau+VuBX8g+IPg==','严经纶',0,NULL,'冰封夕阳','yanjinlun@163.com','浙江省','杭州市','本科','广州中医药大学',NULL,NULL,6),(13,'18766635865','4QrcOUm6Wau+VuBX8g+IPg==','邓和豫',1,NULL,'隐水酣龙','dengheyu@163.com','浙江省','杭州市','本科','哈尔滨工程大学',NULL,NULL,1),(14,'15623527861','4QrcOUm6Wau+VuBX8g+IPg==','邓雪风',0,NULL,'一顿小皮锤','dengxuefeng@163.com','江苏省','南京市','本科','暨南大学',NULL,NULL,4),(16,'17865422925','4QrcOUm6Wau+VuBX8g+IPg==','ZLT',0,1997,'晨钟','babycoder@foxmail.com','浙江省','嘉兴市','本科','青岛大学',2019,'软件工程',5),(17,'15726928003','4QrcOUm6Wau+VuBX8g+IPg==','龟龟',0,NULL,'龟龟','guigui@163.com','广东省','中山市','本科','华东理工大学',NULL,NULL,0),(21,'13957336750','4QrcOUm6Wau+VuBX8g+IPg==','木木',0,NULL,'木木','mumu@163.com','湖北省','武汉市','本科','天津科技大学',NULL,NULL,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -329,4 +356,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-16 11:35:59
+-- Dump completed on 2017-11-16 17:17:38
